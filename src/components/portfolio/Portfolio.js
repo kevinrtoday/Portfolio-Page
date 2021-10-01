d93c4a75aac1e40ec7ac8127b71b8c205087157c
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-09-30 21:18:42 -0400

    Porfolio section sytling and aninmation added

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,9 +1,88 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "real estate",
+      title: "Real Estate",
+    },
+    {
+      id: "finance",
+      title: "Finance",
+    },
+    {
+      id: "travel",
+      title: "Travel",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
-      Hello World
+      <h1> Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://www.linkpicture.com/q/Screen-Shot-2021-09-30-at-4.17.12-PM.png"
+            alt=""
+          />
+          <h3>Crypto App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.linkpicture.com/q/Screen-Shot-2021-09-30-at-4.17.12-PM.png"
+            alt=""
+          />
+          <h3>Crypto App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.linkpicture.com/q/Screen-Shot-2021-09-30-at-4.17.12-PM.png"
+            alt=""
+          />
+          <h3>Crypto App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.linkpicture.com/q/Screen-Shot-2021-09-30-at-4.17.12-PM.png"
+            alt=""
+          />
+          <h3>Crypto App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.linkpicture.com/q/Screen-Shot-2021-09-30-at-4.17.12-PM.png"
+            alt=""
+          />
+          <h3>Crypto App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://www.linkpicture.com/q/Screen-Shot-2021-09-30-at-4.17.12-PM.png"
+            alt=""
+          />
+          <h3>Crypto App</h3>
+        </div>
+      </div>
     </div>
   );
 }
